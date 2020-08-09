bb0079626761ff23f814594cb2aca9459c06198e
AshanFernando69 <fernandoa@stu.otc.school.nz>
2020-08-10 08:57:03 +1200

    I created a list for the options you can type in to 'Hit', I have added different variations of hit in case they spell it wrong or type it in a different way.

--- Programming/Blackjack.py
+++ Programming/Blackjack.py
@@ -21,3 +21,9 @@ Ace = 1
 #Standard 52-card Deck
 
 Cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, Jack, Jack, Jack, Jack, Queen, Queen, Queen, Queen, King, King, King, King, Ace, Ace, Ace, Ace]
+
+#-----------------------#
+
+#Choices for the 'Hit' option
+
+Hit_Choices =  ["Hit","H","h","hit","iht", "thi", "ith", "tih", "hti"]
