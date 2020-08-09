2876eeecc3f23350eafb69937ccf8533ec363309
AshanFernando69 <fernandoa@stu.otc.school.nz>
2020-08-10 08:52:46 +1200

    I have created a list holding all the cards in a standard 52-card deck.

--- Programming/Blackjack.py
+++ Programming/Blackjack.py
@@ -17,3 +17,7 @@ Queen = 10
 Ace = 1
 
 #-----------------------#
+
+#Standard 52-card Deck
+
+Cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, Jack, Jack, Jack, Jack, Queen, Queen, Queen, Queen, King, King, King, King, Ace, Ace, Ace, Ace]
