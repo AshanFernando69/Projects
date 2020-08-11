5b7086bdc8748fa225a3f0feaa1cd861f5faf1f7
AshanFernando69 <fernandoa@stu.otc.school.nz>
2020-08-11 13:17:58 +1200

    To deal the starting hand I have created a 'for' loop that calls each function twice to draw two cards for both the dealer and the player.

--- Programming/Blackjack.py
+++ Programming/Blackjack.py
@@ -1,12 +1,12 @@
-#Imports
+# Imports
 
 import random
 import time
 import sys
 
-#-----------------------#
+# -----------------------#
 
-#Card Values
+# Card Values
 
 Jack = 10
 
@@ -16,41 +16,55 @@ Queen = 10
 
 Ace = 1
 
-#-----------------------#
+# -----------------------#
 
-#Standard 52-card Deck
+# Standard 52-card Deck
 
-Cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, Jack, Jack, Jack, Jack, Queen, Queen, Queen, Queen, King, King, King, King, Ace, Ace, Ace, Ace]
+Cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, Jack, Jack,
+         Jack, Jack, Queen, Queen, Queen, Queen, King, King, King, King, Ace, Ace, Ace, Ace]
 
-#-----------------------#
+# -----------------------#
 
-#Choices for the 'Hit' option
+# Choices for the 'Hit' option
 
-Hit_Choices =  ["Hit","H","h","hit","iht", "thi", "ith", "tih", "hti"]
+Hit_Choices = ["Hit", "H", "h", "hit", "iht", "thi", "ith", "tih", "hti"]
 
-#-----------------------#
+# -----------------------#
 
-#Player & Dealer Deck
+# Player & Dealer Deck
 
 Player_Cards = []
 
 Dealer_Cards = []
 
-#-----------------------#
 
-#Functions
+# -----------------------#
+
+# Functions
 
 def gap():
     print(" ")
 
+
 def getcard_Player():
     player_card = random.choice(Cards)
-    player_Cards.insert(0, Player_card)
+    Player_Cards.insert(0, player_card)
     Cards.remove(player_card)
     print(Player_Cards)
+
 
 def getcard_Dealer():
     dealer_card = random.choice(Cards)
     Dealer_Cards.insert(0, dealer_card)
     Cards.remove(dealer_card)
     print(Dealer_Cards)
+
+# -----------------------#
+
+#Starting Hand
+
+for i in range(2):
+    getcard_Dealer()
+    getcard_Player()
+
+# -----------------------#
